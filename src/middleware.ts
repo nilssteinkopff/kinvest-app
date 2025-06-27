60557831f2c2549b90ca95f7a0407b750f74029d
Nils Steinkopff <nilssteinkopff@MacBook-Air-3.fritz.box>
2025-06-27 21:10:52 +0200

    Stripe

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,12 +9,13 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
+     * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - Any files with extensions (images, etc.)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
